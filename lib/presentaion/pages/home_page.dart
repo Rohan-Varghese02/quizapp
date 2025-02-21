6295ae2db81b1d425834cf53768a3c950654bea8
Rohan Varghese <rohanvarghese02@gmail.com>
2025-02-21 10:16:26 +0530

    Home Screen

---
+++ lib/presentaion/pages/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage('assets/images/quiz-logo.png'),
+              height: 350,
+              width: 350,
+            ),
+            SizedBox(height: 70),
+            Text(
+              'Learn Flutter The fun way!',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(height: 50),
+            OutlinedButton.icon(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              icon: Icon(Icons.arrow_forward_outlined, color: Colors.white),
+              label: Text('Start Quiz', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
